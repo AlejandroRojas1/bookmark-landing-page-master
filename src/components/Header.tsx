6413f69d13fa6d6dc7f12635564ad9f9e02bfbb1
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-10-28 14:40:31 -0300

    Header details adjusted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,20 @@ const Header = () => {
             <nav className="navbar pt-4">
                 <div className={`container ${styles.header}`}>
                     <a className="navbar-brand" href="#">
-                        <img src="/assets/logo-bookmark.svg" alt="bookmark" width="30" height="24" className="w-100" />
+                        <img src="/assets/logo-bookmark.svg" alt="bookmark" width="35" height="30" className="w-100" />
                     </a>
-                    <span className="w-50 d-flex justify-content-evenly align-items-center">
-                        <a>FEATURES</a>
-                        <a>PRICING</a>
-                        <a>CONTACT</a>
+                    <span className="w-50 d-flex fle justify-content-evenly align-items-center">
+                        <ul className='navbar-nav w-75 d-flex flex-row justify-content-evenly'>
+                            <li className='nav-item'>
+                                <a className='nav-link' href='#'>FEATURES</a>
+                            </li>
+                            <li className='nav-item'>
+                                <a className='nav-link' href='#'>PRICING</a>
+                            </li>
+                            <li className='nav-item'>
+                                <a className='nav-link' href='#'>CONTACT</a>
+                            </li>
+                        </ul>
                         <button className={`${styles.button} rounded px-4 py-1`}>Login</button>
                     </span>
                 </div>
