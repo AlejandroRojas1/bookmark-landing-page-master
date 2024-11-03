2a8ddbbd0a57b5ac2662752f85ad2c328eed7840
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-11-03 02:30:45 -0300

    contact component has been simplified

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,13 +3,9 @@ import styles from '../styles/contact.module.css'
 
 const Contact = () => {
     const [mail, setMail] = useState<string>('')
-    const [isValid, setIsValid] = useState<boolean>()
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
         e.preventDefault()
-        const email = e.currentTarget.elements.namedItem('email') as HTMLInputElement;
-
-        setIsValid(email.validity.valid)
     }
 
     const handleChange = (letterEmail: string): void => {
@@ -23,24 +19,15 @@ const Contact = () => {
 
             <form onSubmit={(e): void => { handleSubmit(e) }} className={`${styles.form} mt-4`}>
                 <div className='d-flex w-100 justify-content-center'>
-                    <span className={`${styles.inputContainer} ${!isValid && styles.bginvalidEmail} rounded d-flex flex-column`}>
-                        <input
-                            onChange={(e) => { handleChange(e.target.value) }}
-                            value={mail}
-                            className={`${styles.mail} rounded p-2`}
-                            placeholder="Enter your email address"
-                            type="email"
-                        />
-                        <span className={`${styles.invalidEmail} ${!isValid && styles.insInvalid}`}>
-                            Whoops, make sure it's an email
-                            <img src='/assets/icon-error.svg' />
-                        </span>
-                    </span>
-
+                    <input
+                        onChange={(e) => { handleChange(e.target.value) }}
+                        value={mail}
+                        className={`${styles.mail} rounded p-2`}
+                        placeholder="Enter your email address"
+                        type="email"
+                    />
                     <button type='submit' className={`${styles.button} rounded p-2 ms-2`}>Contact Us</button>
-
                 </div>
-
 
             </form>
         </section>
